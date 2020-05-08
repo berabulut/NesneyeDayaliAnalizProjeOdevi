3ff3b0dad6e834f47924688a987b6c53082e0a20
berabulut <berabulut@gmail.com>
2020-05-08 17:42:45 +0300

    bug

--- src/nyatProje/SicaklikFahrenheit.java
+++ src/nyatProje/SicaklikFahrenheit.java
@@ -10,7 +10,15 @@ public class SicaklikFahrenheit implements ISicaklikBilgisi {
     public String SicaklikDonustur(String sicaklik) {
         if(sicaklik.contains("F"))
             return sicaklik;
-        else
-            return (String.valueOf(Double.parseDouble(sicaklik.substring(0,5)) * 1.8 + 32)).substring(0,4) + "F";
+        else {
+            try {
+                return (String.valueOf(Double.parseDouble(sicaklik.substring(0, 5)) * 1.8 + 32)).substring(0, 4) + "F";
+            }
+            catch(java.lang.NumberFormatException e) {
+                return (String.valueOf(Double.parseDouble(sicaklik.substring(0, 4)) * 1.8 + 32)).substring(0, 4) + "F";
+            }
+
+        }
+
     }
 }
